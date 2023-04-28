6da21e29899056529c47194ab4c90b376d0675bd
ernestberziu <eberziu1@gmail.com>
2023-04-28 03:15:09 +0200

    asssad

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 const app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(cors({
   origin: '*'
