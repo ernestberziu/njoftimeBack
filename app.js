c359dcedd99da052275ac858bee77edc76e1a233
ernestberziu <eberziu1@gmail.com>
2023-05-02 00:46:07 +0200

    sad

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+const mongoose = require('mongoose');
 const cors = require('cors');
-const { isPunemarres } = require('./middleware')
-var indexRouter = require('./routes/index');
 const cookieParser = require('cookie-parser');
-var productsRouter = require('./routes/products');
-var usersRouter = require('./routes/users');
-const API = require('./db');
-require('dotenv').config();
 
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.error('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));;
 
 const app = express();
 
@@ -27,10 +26,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/products', isPunemarres, productsRouter);
-app.use('/users', usersRouter);
-app.post('/login', API.login);
 app.use("/api/products", require("./api/products"));
 app.use("/api/customers", require("./api/customers"));
 app.use("/api/categories", require("./api/categories"));
@@ -39,21 +34,10 @@ app.use("/api/users", require("./api/users"));
 app.use("/api", require("./api/transactions"));
 
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 
 module.exports = app;
